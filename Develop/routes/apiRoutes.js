6b34eb4cd3633ae1d2c4009dcea99cc22b789362
Linus41 <schief.craft@gmail.com>
2020-04-22 13:46:05 -0700

    retrieve notes and add notes is working

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,28 +4,38 @@ var fs = require("fs");
 module.exports = function (app) {
     // /api/notes reads the db.json file and returns all saved notes as JSON
     app.get("/api/notes", function (req, res) {
-        console.log("res", res);
-        //did I enter db.json correctly below?
-        // res.readFile(path.join(__dirname, "../db/db.json"));
+        // console.log("res", res);
         fs.readFile("./db/db.json", "utf8", (err, response) => {
-            if(err) throw err;
-            console.log("read json file/response", response)
+            if (err) throw err;
+            // console.log("read json file/response", response)
             var allNotes = [];
             allNotes = JSON.parse(response);
+            // console.log(allNotes);
             res.json(allNotes);
         })
     });
 
     // /api/notes receives new note to save on the req body, adds to db.json file, returns new note to client
     app.post("/api/notes", function (req, res) {
-        var reqBody = req.body;
-        
-        // res.sendFile(path.join(__dirname, "../db/db.json"));
-        fs.appendFile("./db/db.json", "utf8", reqBody, (err, response) => {
+        //receive new note from req body, save it in variable
+        let giveBack = req.body
+        fs.readFile("./db/db.json", "utf8", (err, response) => {
             if (err) throw err;
-            res.json(req.body);
-            
+            // parse response into json and save to allNotes
+            let allNotes = JSON.parse(response);
+            // console.log(allNotes);
+            // now allNotes becomes itself plus the new note
+            allNotes = [...allNotes, giveBack]
+            console.log("combined list", allNotes);
+            //then we write new combined list (allNotes) back to db.json  file        
+            fs.writeFile("./db/db.json", JSON.stringify(allNotes), (err) => {
+                if (err) throw err;
+                res.json({ success: true, msg: 'Created new note' });
+                console.log("Note created!", giveBack);
+
+            })
         })
+        //return new note to client
     });
     //example from Starwars 12 activity: 
 
